c101ca6e95b4926d4ee769a061fbff39a18b12d0
twosson <zebedaios.li@gmail.com>
2023-10-21 17:22:38 +0800

    init project

--- cmd/client.go
+++ cmd/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"fmv/pkg/client"
+	"fmv/pkg/consul"
 	"github.com/gogf/gf/v2/os/gfile"
 	"github.com/gogf/gf/v2/os/gtimer"
 	"github.com/urfave/cli/v2"
@@ -48,14 +49,17 @@ var clientCmd = &cli.Command{
 		},
 	},
 	Action: func(ctx *cli.Context) error {
-		consul := ctx.String("consul")
+		consulAddr := ctx.String("consul")
 		tag := ctx.String("tag")
 		chunk := ctx.Int("chunk")
 		path := ctx.String("path")
 		suffix := ctx.String("suffix")
 		parallel := ctx.Int("parallel")
 
+		ch := make(chan struct{}, parallel)
+
 		gtimer.AddSingleton(ctx.Context, time.Second, func(ctx context.Context) {
+			ch <- struct{}{}
 			list, err := gfile.ScanDirFile(path, suffix, false)
 			if err != nil {
 				fmt.Printf("error (%s) in obtaining file list", err.Error())
@@ -67,10 +71,20 @@ var clientCmd = &cli.Command{
 				return
 			}
 
-			if len(list) > parallel {
-				list = list[:parallel-1]
-			}
-			client.StartClient(list, chunk, consul, tag)
+			go func(file string) {
+				se, err := consul.Discovery("fmv-server", consulAddr, tag)
+				if err != nil {
+					fmt.Printf("discovery error: %s", err.Error())
+				}
+				se, err = consul.Discovery("fmv-server", consulAddr, "")
+				if err != nil {
+					fmt.Printf("discovery error: %s", err.Error())
+					return
+				}
+				fc := client.NewFileClient(file, se, chunk)
+				fc.SendFile()
+				<-ch
+			}(list[0])
 		})
 
 		select {}
